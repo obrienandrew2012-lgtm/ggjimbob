e68c60cfa6a82131b69ac8d57a472ff70361889b
Qwertz Studio <obrienandrew2012@gmail.com>
2026-01-12 14:46:03 -0600

    game.py

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 print("hello welcome to ggjimbob")
-input("please end my suffering")
-g1 = ("yes or no ")
+input("please end my suffering ")
 print(g1)
+g1 = ("yes or no ")
 tubular = input("hooray ")
 if g1 == "yes": print(tubular)
 
@@ -12,7 +12,6 @@ elif g1 == "no":print("WHY MUST YOU DO THIS TO ME")
 print("ok so now that you've answered i will now proceed")
 fibal = input("TO DO THIS! F I R E B A L L ! ! !  ")
 print(fibal)
-print("your options on what to say, 'what', 'yikes', and 'i block it'
 if fibal == "what": print("yes")
 elif fibal == "yikes": print("T R E M B L E  I N  F E A R ")
 elif fibal == "i block it": print("no you dont")
