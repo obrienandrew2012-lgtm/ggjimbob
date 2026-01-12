6a1326eccfa07a42a073b39e9136cb3644177692
Qwertz Studio <obrienandrew2012@gmail.com>
2026-01-12 14:26:22 -0600

    game.py

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
-print("hello welcome to coolgame")
+print("hello welcome to ggjimbob")
 input("please end my suffering")
 g1 = ("yes or no")
 print(g1)
 
-if g1 = "yes": print("thank you")
+if g1 == "yes": print("thank you")
 
-elif g1 = "jimbob": print("nuh uh")
+elif g1 == "jimbob": print("nuh uh")
 
