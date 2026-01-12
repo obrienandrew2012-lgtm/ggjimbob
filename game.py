56bd6ff11ec7a7da11f8f63c805252f7547d4cad
Qwertz Studio <obrienandrew2012@gmail.com>
2026-01-12 14:40:52 -0600

    betagame.py
    its still in beta

--- game.py
+++ game.py
@@ -1,9 +1,18 @@
 print("hello welcome to ggjimbob")
 input("please end my suffering")
-g1 = ("yes or no")
+g1 = ("yes or no ")
 print(g1)
-
-if g1 == "yes": print("thank you")
+tubular = input("hooray ")
+if g1 == "yes": print(tubular)
 
 elif g1 == "jimbob": print("nuh uh")
 
+elif g1 == "no":print("WHY MUST YOU DO THIS TO ME")
+
+print("ok so now that you've answered i will now proceed")
+fibal = input("TO DO THIS! F I R E B A L L ! ! !  ")
+print(fibal)
+print("your options on what to say, 'what', 'yikes', and 'i block it'
+if fibal == "what": print("yes")
+elif fibal == "yikes": print("T R E M B L E  I N  F E A R ")
+elif fibal == "i block it": print("no you dont")
